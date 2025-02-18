99a51ce9a3a3562a9db068ff98f237e7b4d4c79a
Md Altamash Ansari <132382652+mdaltamashansari@users.noreply.github.com>
2025-02-18 22:19:06 +0530

    Add files via upload

---
+++ JavaScript-GFG/03 - Functions/1. function declaration.js
@@ -0,0 +1,19 @@
+/**
+ * Functions 🔥🔥🔥
+ * 1. Function Statement/Function Declaration
+ * 
+ */
+
+
+
+// Function Declaration or Function Statement
+function greetMessage(name, city){ // name is called as a parameter
+    console.log(`${name}, welcome to geeksforgeeks`);
+    console.log(`Thank you for joining from ${city}`)
+}
+
+
+//Calling the function
+greetMessage("Altamash", "Mumbai"); // Altamash is called as an argument
+greetMessage("Aftab", "kaif");
+greetMessage("imran");

1ec2edfb7e6cbf9a9bc15b5194cf898f23c76b8c
My-Codes <mdaltamash075@gmail.com>
2025-10-18 20:10:09 +0530

    add

--- 100 Days of code: Python Bootcamp/Day 4/who_will_pay_end.py
+++ 100 Days of code: Python Bootcamp/Day 4/who_will_pay_end.py
@@ -2,3 +2,6 @@ friends = ['Altamash', 'Ahmed', 'Saeed', 'Hassan', 'Umair']
 import random
 pay = random.choice(friends)
 print(f"{pay} is going to buy the meal today!")
+
+random_index = random.randint(0, 4)
+print(f"{friends[random_index]} is going to buy the meal today!")

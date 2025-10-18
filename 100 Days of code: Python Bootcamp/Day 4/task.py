211b328d39c0cc8284f7a22e14bfaa2d57da463a
My-Codes <mdaltamash075@gmail.com>
2025-10-18 19:08:17 +0530

    add

---
+++ 100 Days of code: Python Bootcamp/Day 4/task.py
@@ -0,0 +1,8 @@
+# random number generator by random module
+import random
+import my_module  #imprting my_module.py file 
+
+random_number = random.randint(1, 10)
+print(random_number)
+
+print(my_module.my_favorite_number)

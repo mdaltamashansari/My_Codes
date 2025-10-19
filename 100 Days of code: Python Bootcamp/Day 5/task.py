faafac3de7a29f6b55de8573af2eeb519a0e9257
My-Codes <mdaltamash075@gmail.com>
2025-10-19 13:58:54 +0530

    add

--- 100 Days of code: Python Bootcamp/Day 5/task.py
+++ 100 Days of code: Python Bootcamp/Day 5/task.py
@@ -1,1 +1,6 @@
-#Loop
+#Altamash Codes
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+      print(fruit)
+      print(fruit + " pie") #adds " pie" to each fruit name
+print(fruits) #prints the original list

2a5300fad99739772cebbe3d8f3581305c9e0de4
My-Codes <mdaltamash075@gmail.com>
2025-10-20 13:11:45 +0530

    add

--- 100 Days of code: Python Bootcamp/Day 5/task2.py
+++ 100 Days of code: Python Bootcamp/Day 5/task2.py
@@ -1,3 +1,4 @@
+#Altamash Codes
 student_scores = [97, 65, 88, 12, 45, 78, 99, 100, 54, 23, 67, 34, 89, 90, 91, 92, 93, 94, 95, 96]
 
 for score in student_scores:
@@ -15,8 +16,7 @@ for number in range(1, 101):
    total += number
 print(total)
 
-
-# print 1 to 100 
+# print 1 to 100 and replace multiples of 3 with Fizz, multiples of 5 with Buzz and multiples of both with FizzBuzz
 for number in range(1, 101):
  if number % 3 == 0 and number % 5 == 0:
       print("FizzBuzz")

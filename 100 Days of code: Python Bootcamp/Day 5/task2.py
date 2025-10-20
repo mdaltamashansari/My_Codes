f1846e024b455959a904675c86759a9941cb8e4b
My-Codes <mdaltamash075@gmail.com>
2025-10-20 12:57:35 +0530

    add

--- 100 Days of code: Python Bootcamp/Day 5/task2.py
+++ 100 Days of code: Python Bootcamp/Day 5/task2.py
@@ -1,1 +1,28 @@
-student_score = [97, 65, 88, 12, 45, 78, 99, 100, 54, 23, 67, 34, 89, 90, 91, 92, 93, 94, 95, 96]
+student_scores = [97, 65, 88, 12, 45, 78, 99, 100, 54, 23, 67, 34, 89, 90, 91, 92, 93, 94, 95, 96]
+
+for score in student_scores:
+      sum = 0
+      sum += score
+print(sum)
+
+# for loop range function
+for number in range(1, 10):
+      print(number)
+      
+# add 1 to 100 
+total = 0
+for number in range(1, 101):
+   total += number
+print(total)
+
+
+# print 1 to 100 
+for number in range(1, 101):
+ if number % 3 == 0 and number % 5 == 0:
+      print("FizzBuzz")
+ elif number % 3 == 0:
+      print("Fizz")
+ elif number % 5 == 0:
+      print("Buzz")
+ else:
+      print(number)

72e3822d4488c4149b71215d312b1bbcab446842
My-Codes <mdaltamash075@gmail.com>
2025-10-22 13:54:25 +0530

    add

--- 100 Days of code: Python Bootcamp/Day 6/task.py
+++ 100 Days of code: Python Bootcamp/Day 6/task.py
@@ -1,3 +1,18 @@
 print("hello")
 char_lens = len("hello")
 print(char_lens)
+
+# #define function 
+# def my_function():
+      # do this 
+      # then do this 
+      # finally do this
+      
+# #call function
+# my_function()
+# identention      
+# def one_move():
+#     move()
+#     turn_left()
+#     move()
+#     use space or tab for indentation

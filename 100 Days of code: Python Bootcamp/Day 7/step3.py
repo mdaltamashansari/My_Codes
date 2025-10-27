1877a994beecf713bb403cf89db34b244e3361ef
My-Codes <mdaltamash075@gmail.com>
2025-10-27 19:43:05 +0530

    add

--- 100 Days of code: Python Bootcamp/Day 7/step3.py
+++ 100 Days of code: Python Bootcamp/Day 7/step3.py
@@ -1,1 +1,7 @@
-# error  step 3 
+word_list = ["ardvark", "baboon", "camel"]
+import random 
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+guess = input("Guess a letter: ").lower()
+print(f"You guessed: {guess}") 

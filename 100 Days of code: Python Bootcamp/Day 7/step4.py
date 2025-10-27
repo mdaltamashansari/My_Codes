085f6e394bfbe827d7eba7cee81fc66187c01352
My-Codes <mdaltamash075@gmail.com>
2025-10-28 00:07:36 +0530

    added

--- 100 Days of code: Python Bootcamp/Day 7/step4.py
+++ 100 Days of code: Python Bootcamp/Day 7/step4.py
@@ -1,5 +1,62 @@
 import random
-word_list = ["aardvark", "baboon", "camel"]
+stages = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+word_list = ('ant baboon badger bat bear beaver camel cat clam cobra cougar '
+         'coyote crow deer dog donkey duck eagle ferret fox frog goat '
+         'goose hawk lion lizard llama mole monkey moose mouse mule newt '
+         'otter owl panda parrot pigeon python rabbit ram rat raven '
+         'rhino salmon seal shark sheep skunk sloth snake spider '
+         'stork swan tiger toad trout turkey turtle weasel whale wolf '
+         'wombat zebra ').split()
+lives = 6
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
@@ -27,6 +84,13 @@ while not gameover:
             display += "_"  
             
       print(display)
+      
+      if guess not in chosen_word:
+            lives -= 1
+            if lives == 0:
+                gameover = True
+                print("You lose.")
       if "_" not in display:
             gameover = True
-            print("You win!")
+            print("You win!")       
+      print (stages[lives])

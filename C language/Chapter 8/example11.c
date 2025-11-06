3377bc748ba1f581d2988ef2816c0e6e68f3f94e
Md Altamash Ansari <132382652+mdaltamashansari@users.noreply.github.com>
2025-11-06 13:16:22 +0530

    Update example11.c

--- C language/Chapter 8/example11.c
+++ C language/Chapter 8/example11.c
@@ -1,26 +1,20 @@
 //WAP  to encrypt a string by adding 1 to the ascii value of its characters.
 //WAP to decrypt the string encrypted using encrypt function in previous ques.
-
 #include <stdio.h>
-
 void decrypt(char*str){
     int i=0;
     while (str[i]!='\0'){
         str[i] = str[i] - 1;
         i++;
-    }
-    
+    }  
 }
-
 void encrypt(char*str){
     int i=0;
     while (str[i]!='\0'){
         str[i] = str[i] + 1;
         i++;
-    }
-    
+    }   
 }
-
 int main()
 {
     char str[] = "The password is hello world";
@@ -30,4 +24,5 @@ int main()
     decrypt(str);
     printf("decrypted string: %s\n",str);
     return 0;
+
 }

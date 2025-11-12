555b8b644ed151cca02835b211555094aaf63e11
Md Altamash Ansari <132382652+mdaltamashansari@users.noreply.github.com>
2025-11-12 19:02:40 +0530

    Update ap.c

--- C OneShot/loop/ap.c
+++ C OneShot/loop/ap.c
@@ -4,8 +4,9 @@ int main()
     int n;
     printf("Enter Number here: ");
     scanf("%d",&n);
-    for (int i = 1;        ; i++)
+    for (int i = 1;       ; i++)
     {
         printf("%d\n",i);
     }
 }
+
